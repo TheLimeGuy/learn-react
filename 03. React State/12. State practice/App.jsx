3ceb4a21247f54d2c066d17bee8b2b35188eea6c
Liam <technical@albyte.co.za>
2025-06-04 15:10:47 +0200

    Remove unnecessary comments related to state management in App component

--- 03. React State/12. State practice/App.jsx
+++ 03. React State/12. State practice/App.jsx
@@ -1,12 +1,7 @@
 import React from "react"
 
 export default function App() {
-    let [stateCount,setStateCount] = React.useState(0)
-    /**
-     * Challenge: 
-     * Create state to track our count value (initial value is 0)
-     * Don't forget to replace the hard-coded "0" with your new state
-     */
+    let [stateCount,setStateCount] = React.useState(0) 
     return (
         <main className="container">
             <h1>How many times will Bob say "state" in this section?</h1>
