448b56c583b9e54cd6e9a835f1f8b6caf586f4dd
Liam <technical@albyte.co.za>
2025-06-04 14:52:04 +0200

    Implement code changes to enhance functionality and improve performance

--- 03. React State/09. useState/App.jsx
+++ 03. React State/09. useState/App.jsx
@@ -2,18 +2,14 @@ import React from "react"
 
 export default function App() {
     
-    /**
-     * Challenge: Replace our hard-coded "Yes" on the page with 
-     * some state initiated with React.useState()
-     */
-    
+    const whatever = React.useState("Yes")
     const result = React.useState("Hello")
     console.log(result)
     
     return (
         <main>
             <h1 className="title">Is state important to know?</h1>
-            <button className="value">Yes</button>
+            <button className="value">{whatever[0]}</button>
         </main>
     )
 }
