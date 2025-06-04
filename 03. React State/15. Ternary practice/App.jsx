e3dc14fa81b11f164941ddaea2a47da1757e6240
Liam <technical@albyte.co.za>
2025-06-04 16:14:38 +0200

    Completed Challenge

--- 03. React State/15. Ternary practice/App.jsx
+++ 03. React State/15. Ternary practice/App.jsx
@@ -1,23 +1,18 @@
+import React from 'react'
 export default function App() {
-    /**
-     * Challenge: Replace the if/else below with a ternary
-     * to determine the text that should display on the page
-     */
-    const isGoingOut = true
-    
-    let answer  // 👈 Use ternary here
-    
-    // Remove the code below 👇 once your ternary is done
-    if(isGoingOut === true) {
-        answer = "Yes"
-    } else {
-        answer = "No"
-    }
-    
+    const [isGoingOut, setIsGoingOut] = React.useState("Yes")
+    function handleClick(){
+        (isGoingOut=="Yes") ? setIsGoingOut("No") : setIsGoingOut("Yes")
+    }   
     return (
         <main>
             <h1 className="title">Do I feel like going out tonight?</h1>
-            <button className="value">{answer}</button>
+            <button 
+            className="value"
+            onClick={handleClick}
+            >
+                {isGoingOut}
+            </button>
         </main>
     )
 }
