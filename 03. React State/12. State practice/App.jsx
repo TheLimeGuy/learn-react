3b25c179906703c73c3c2c12f98fc08fcfa654db
Liam <technical@albyte.co.za>
2025-06-04 15:15:24 +0200

    Refactor button components to include click handlers and change stateCount declaration to const

--- 03. React State/12. State practice/App.jsx
+++ 03. React State/12. State practice/App.jsx
@@ -1,14 +1,32 @@
 import React from "react"
 
 export default function App() {
-    let [stateCount,setStateCount] = React.useState(0) 
+    const [stateCount,setStateCount] = React.useState(0) 
+    function handleClickPlus(){
+        console.log("ADD")
+    }
+    function handleClickMinus(){
+        console.log("MINUS")
+    }
     return (
         <main className="container">
             <h1>How many times will Bob say "state" in this section?</h1>
             <div className="counter">
-                <button className="minus" aria-label="Decrease count">–</button>
+                <button 
+                    className="minus" 
+                    aria-label="Decrease count"
+                    onClick={handleClickMinus}
+                >
+                        –
+                </button>
                 <h2 className="count">{stateCount}</h2>
-                <button className="plus" aria-label="Increase count">+</button>
+                <button 
+                    className="plus" 
+                    aria-label="Increase count"
+                    onClick={handleClickPlus}
+                >
+                    +
+                </button>
             </div>
         </main>
     )
