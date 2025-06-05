e725230c37c75678018bdccc05db10d1e5e88e63
Liam <technical@albyte.co.za>
2025-06-05 11:04:04 +0200

    Completed Challenge

--- 03. React State/22. Form basics/index.jsx
+++ 03. React State/22. Form basics/index.jsx
@@ -14,7 +14,8 @@ function App() {
         <input id="email" type="email" name="email" placeholder="joe@schmoe.com" />
         <br />
         
-        
+        <label htmlFor="password">Password:</label>
+        <input id="password" type="password" name="password"/>
         
       </form>
     </section>
